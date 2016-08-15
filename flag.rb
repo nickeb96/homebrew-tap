d8323783fc84e03a42e771e5d2c2269d4bacdf05
nickeb96 <nickboyle5496@gmail.com>
2016-08-14 21:02:21 -0400

    Fixed flag formula

--- flag.rb
+++ flag.rb
@@ -2,7 +2,7 @@ class Flag < Formula
   desc ""
   homepage "https://github.com/nickeb96/flag"
   url "https://github.com/nickeb96/flag/archive/1.0.0.tar.gz"
-  version "1.0.0"
+  #version "1.0.0"
   sha256 "5da5d8c80252a516a3007f5ec772b9f2dec247259e45887592a936f1c3376d50"
 
   def install
