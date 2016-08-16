8e4993ac332b72e768fc4f8212b588a1c0c4c4aa
nickeb96 <nickboyle5496@gmail.com>
2016-08-16 17:15:38 -0400

    Updated resolve formula

--- resolve.rb
+++ resolve.rb
@@ -1,8 +1,8 @@
 class Resolve < Formula
   desc ""
   homepage "https://github.com/nickeb96/resolve"
-  url "https://github.com/nickeb96/resolve/archive/1.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/nickeb96/resolve/archive/1.0.1.tar.gz"
+  #version "1.0.0"
   sha256 "475b9d711912e4d2e3644aff54c18784053cc6b1dd7935d43f2e536ef6974e86"
 
   def install
