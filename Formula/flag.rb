0174bf3ccc588c9e6403ce6c067bf48cf6b7bd68
nickeb96 <nickboyle5496@gmail.com>
2016-11-13 14:28:58 -0500

    Updated flag to 1.0.2

--- Formula/flag.rb
+++ Formula/flag.rb
@@ -1,8 +1,7 @@
 class Flag < Formula
   desc ""
   homepage "https://github.com/nickeb96/flag"
-  url "https://github.com/nickeb96/flag/archive/1.0.1.tar.gz"
-  #version "1.0.0"
+  url "https://github.com/nickeb96/flag/archive/1.0.2.tar.gz"
   sha256 "a01e4cbe98ab472e69f7869c660cc6a75aec34c7151c2f59c5753d63a7eb799a"
 
   def install
